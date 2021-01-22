9a5e1121aae292fde9289de8da2977e1e212c3f3
Tathus Theerachuwiwat <phol-2002@hotmail.com>
2021-01-22 06:01:38 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Subject:\t\tComputer Programming\tSection:\t1\n");
+    printf("ID:\t\t\t\t58079999\t\t\t\tName:\t\tSomchai Somying");
+    return 0;
+}
+  
